34700f14476ad893135a30051d7764e1a9412480
Aren Sinaei <arensinani7@gmail.com>
2023-08-30 12:31:47 +0400

    config routes

--- App/http/controllers/api/home.controller.js
+++ App/http/controllers/api/home.controller.js
@@ -2,7 +2,7 @@ const Controller = require("../controller");
 
 class HomeController extends Controller {
   indexPage(req, res, next) {
-    return res.status(200).send("Index page store");
+    return res.status(200).send("Index page stored");
   }
 }
 
