d2c6c4e3feb6084a2ea77eed8e2f002bc7c7fa28
arensin8 <arensinani4@gmail.com>
2024-02-23 22:58:39 +0400

    chat room connctions creating

--- public/dist/socket.js
+++ public/dist/socket.js
@@ -1,22 +1,29 @@
 const socket = io("http://localhost:3000");
 
+function stringToHTML(str) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, "text/html");
+  return doc.body.firstChild;
+}
+
 function initNamespaceConnection(endpoint) {
   const namespaceSocket = io(`http://localhost:3000/${endpoint}`);
   namespaceSocket.on("connect", () => {
     namespaceSocket.on("roomList", (rooms) => {
+      const roomsElement = document.querySelector("#contacts ul");
+      roomsElement.innerHTML = "";
       for (const room of rooms) {
-        const roomsElement = document.querySelector("#contacts ul");
-        roomsElement.appendChild(`
-        <li class="contact" roomName="${room.name}">
-        <div class="wrap">
-            <img src="${room.image}" height="40"/>
-            <div class="meta">
-                <p class="name">${room.name}</p>
-                <p class="preview" >${room.description}</p>
+        const parsedHtml = stringToHTML(`
+            <li class="contact" roomName="${room.name}">
+            <div class="wrap">
+                <img src="${room.image}" height="40"/>
+                <div class="meta">
+                    <p class="name">${room.name}</p>
+                    <p class="preview" >${room.description}</p>
+                </div>
             </div>
-        </div>
-    </li>
-        `);
+            </li>`);
+        roomsElement.appendChild(parsedHtml);
       }
     });
   });
@@ -29,9 +36,21 @@ socket.on("connect", () => {
     for (const namespace of namespacesList) {
       const li = document.createElement("li");
       const p = document.createElement("p");
+      p.setAttribute("class", "namespaceTitle");
+      p.setAttribute("endpoint", namespace.endpoint);
       p.innerText = namespace.title;
       li.appendChild(p);
       namespaceElement.appendChild(li);
+    }
+    const namespaceNodes = document.querySelectorAll(
+      "#namespaces li p.namespaceTitle"
+    );
+    console.log(namespaceNodes);
+    for (const namespace of namespaceNodes) {
+      namespace.addEventListener("click", () => {
+        const endpoint = namespace.getAttribute("endpoint");
+        initNamespaceConnection(endpoint);
+      });
     }
   });
 });
